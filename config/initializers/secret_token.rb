f1becb4933456a3a9f51bc5c588232f4030efb98
Linus Oleander <linus@oleander.nu>
2011-05-26 03:18:04 +0200

    Creating project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackLocationApp::Application.config.secret_token = 'f4598a28901b27cc177a5342ecc95aac9275f6b83efcc1d54bfb99833ad264f487a8f39027c259efce6a2b3d8eb6711d697e9f983ee91c38fc421cc513b5dbf9'
